9d030a0dd5aac78c2c0a9985ee109f6c73cc46cc
Patrick Martin <pmartt10@gmail.com>
2020-03-10 18:17:52 -0400

    mostly finished

--- echo.py
+++ echo.py
@@ -2,21 +2,48 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "Patrick Martin w/ help"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    # Creating initial parser
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.')
+    # Adding arguments to parser ('upper', 'lower', 'title' options)
+    parser.add_argument("text", help="text to be manipulated")
+
+    parser.add_argument("-u", "--upper", help="Convert text to uppercase",
+                        action="store_true")
+    parser.add_argument("-l", "--lower", help="Convert text to lowercase",
+                        action="store_true")
+    parser.add_argument("-t", "--title", help="Convert text to titlecase",
+                        action="store_true")
+
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    parser = create_parser()  # Creates  parser instance
+    x = parser.parse_args(args)
+    result_text = x.text
+    # if value is upper/lower/title
+    if x.upper:
+        result_text = result_text.upper()  # using uppercase method to convert text
+
+    if x.lower:
+        result_text = result_text.lower()  # using lowercase method to convert text
+
+    if x.title:
+        result_text = result_text.title()  # using .title method to convert text
+
+    return result_text
 
 
 if __name__ == '__main__':
-    pass
+    print(main(sys.argv[1:]))
